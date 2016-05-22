57948605c31e002fa4013a0f9fc0642a5810e998
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-05-22 17:48:12 -0400

    (SIMP-1123) Added Fedora 23 Facts
    The collection script and Vagrantfile were updated to (hopefully) work
    properly between both EL and Fedora systems.
    
    The script was updated to bring in all necessary prerequisites from a
    base system as well as add --nogpgcheck for the yum/dnf installations of
    new RPMs.
    
    SIMP-1123 #close
    
    Change-Id: I39005865a62154863167be57e5998f7b28ca9ee7

--- facts/scripts/get_facts_for_each_facter.sh
+++ facts/scripts/get_facts_for_each_facter.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
-operatingsystemmajrelease=$1
+operatingsystem=$( echo "$1" | cut -f1 -d' ' )
+operatingsystemmajrelease=$( echo "$1" | cut -f2 -d' ' )
+
+if [ -z "$operatingsystem" ] || [ -z "$operatingsystemmajrelease" ]; then
+  echo "You must pass an operating system and version to $0"
+  exit 1
+fi
+
+if [ "$operatingsystem" == "$operatingsystemmajrelease" ]; then
+  echo "Your OS is the same as your version and this does not make sense"
+  exit 1
+fi
+
 export PATH=/opt/puppetlabs/bin:$PATH
 export FACTERLIB=`ls -1d /vagrant/modules/*/lib/facter | tr '\n' ':'`
 
-wget "https://yum.puppetlabs.com/puppetlabs-release-pc1-el-${operatingsystemmajrelease}.noarch.rpm" -O /tmp/puppetlabs-release-pc1.rpm
-rpm -ivh /tmp/puppetlabs-release-pc1.rpm
+which dnf > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  rpm_cmd='dnf --best --allowerasing'
+else
+  rpm_cmd='yum --skip-broken'
+fi
 
-# Because `yum install epel-release -y` doesn't work for RedHat:
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-${operatingsystemmajrelease}.noarch.rpm
-rpm -Uvh epel-release-latest-${operatingsystemmajrelease}*.rpm
+if [ "${operatingsystem}" != 'fedora' ]; then
+  plabs_ver='el'
 
+  $rpm_cmd install -y --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-${operatingsystemmajrelease}.noarch.rpm
+else
+  plabs_ver=$operatingsystem
+fi
+
+$rpm_cmd install -y --nogpgcheck "https://yum.puppetlabs.com/puppetlabs-release-pc1-${plabs_ver}-${operatingsystemmajrelease}.noarch.rpm"
+
+# Prereqs
+$rpm_cmd install -y facter rubygem-bundler git augeas-devel \
+  libicu-devel libxml2 libxml2-devel libxslt libxslt-devel \
+  gcc gcc-c++ ruby-devel
+
+$rpm_cmd install -y puppet-agent || $rpm_cmd install -y puppet
 
 # Capture data for (c)facter 3.X
 for puppet_agent_version in 1.2.2 1.2.7; do
-  yum install -y puppet-agent-${puppet_agent_version} rubygems git ruby-devel
-  output_dir="/vagrant/$(facter --version | cut -c1-3)"
-  output_file="$(facter operatingsystem | tr '[:upper:]' '[:lower:]')-$(facter operatingsystemmajrelease)-$(facter hardwaremodel).facts"
+  output_dir="/vagrant/$( facter --version | cut -c1-3 )"
+  output_file="$( facter operatingsystem | tr '[:upper:]' '[:lower:]' )-$( facter operatingsystemmajrelease )-$( facter hardwaremodel ).facts"
   mkdir -p $output_dir
   puppet facts | tee "${output_dir}/${output_file}"
 done
 
-operatingsystem=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
-operatingsystemmajrelease=$(facter operatingsystemmajrelease)
-hardwaremodel=$(facter hardwaremodel)
+operatingsystem=$( facter operatingsystem | tr '[:upper:]' '[:lower:]' )
+operatingsystemmajrelease=$( facter operatingsystemmajrelease )
+hardwaremodel=$( facter hardwaremodel )
 
 PATH=/opt/puppetlabs/puppet/bin:$PATH
 gem install bundler --no-ri --no-rdoc --no-format-executable
@@ -38,6 +65,6 @@ for version in 1.7.0 2.0.0 2.1.0 2.2.0 2.3.0 2.4.0; do
   echo
   echo
   output_file="/vagrant/${os_string}.facts"
-  mkdir -p $(dirname $output_file)
+  mkdir -p $( dirname $output_file )
   FACTER_GEM_VERSION="~> ${version}" bundle exec ruby /vagrant/scripts/get_facts.rb | tee $output_file
 done
